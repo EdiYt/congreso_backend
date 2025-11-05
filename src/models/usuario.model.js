661dcbd87e42dfb79c3574a8338791a3f2004932
Erick <erickvillaseor@yahoo.com.mx>
2025-11-04 18:45:46 -0600

    Se creo el get by id de los participantes

--- src/models/usuario.model.js
+++ src/models/usuario.model.js
@@ -4,3 +4,8 @@ async function getAllParticipantes(){
     const [rows] = await pool.query('SELECT * FROM participante');
     return rows;
 }
+
+async function getParticipanteById(id){
+    const [rows] = await pool.query('SELECT * FROM participante WHERE participante_id = ?', [id]);
+    return rows[0];
+}
