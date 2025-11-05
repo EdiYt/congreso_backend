4e80a3d9ee32eda6dacfd943d1cc596917ecfb82
Erick <erickvillaseor@yahoo.com.mx>
2025-11-04 18:57:35 -0600

    Se creo el registro de participantes

--- src/models/usuario.model.js
+++ src/models/usuario.model.js
@@ -9,3 +9,17 @@ async function getParticipanteById(id){
     const [rows] = await pool.query('SELECT * FROM participante WHERE participante_id = ?', [id]);
     return rows[0];
 }
+
+async function createParticipante(data){
+    const query = 'INSERT INTO participante (participante_id, nombre, apellidos, email, usuario_twitter, ocupacion VALUES (?,?,?,?,?,?))';
+    const [result] = await pool.query(query, [
+        data.participante_id,
+        data.nombre,
+        data.apellidos,
+        data.email,
+        data.usuario_twitter,
+        data.ocupacion
+    ])
+
+    return { id: result.insertId }
+}
