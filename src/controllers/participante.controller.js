3bfa40a3ed3943c593f30c9299cc483154f7f1dc
Erick <erickvillaseor@yahoo.com.mx>
2025-11-05 13:21:38 -0600

    Funcion de get participantes por id agregada

--- src/controllers/participante.controller.js
+++ src/controllers/participante.controller.js
@@ -8,3 +8,16 @@ exports.getParticipantes = async (req, res) => {
         res.status(500).json({ error: 'Error al obtener a todos los participantes'})
     }
 };
+
+exports.getParticipantesById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const participante = await Participante.getParticipanteById(id);
+        if (!participante) {
+            return res.status(404).json({ error: 'Participante no encontrado' })
+        }
+        res.json(participante);
+    } catch (err) {
+        res.status(500).json({ error: 'Error al obtener participante por ID' })
+    }
+};
