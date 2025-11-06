3b59e2eaf6663c7be2b28d1edbe8323efe0c72e6
Erick <erickvillaseor@yahoo.com.mx>
2025-11-06 12:22:55 -0600

    Se arreglo un error que no encontraba la carpeta para guardar las imagenes de avatars

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -3,10 +3,9 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '/uploads/avatars'); 
+    cb(null, path.join(__dirname, '../../uploads/avatars'));
   },
   filename: (req, file, cb) => {
-    
     const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9);
     cb(null, uniqueName + path.extname(file.originalname));
   },
