47ddfe22a29d1eab3759282c3f0722e136b0eaa7
Erick <erickvillaseor@yahoo.com.mx>
2025-11-06 00:16:20 -0600

    Se agrego el cors

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const participanteRoutes = require('./src/routes/participante.route');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', participanteRoutes);
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en puerto ${PORT}`);
 });
